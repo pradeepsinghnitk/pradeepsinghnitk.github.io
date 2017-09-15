f55acdd16a6ce3eefba70322f5197cb1d9ceec44
PRADEEP SINGH <pradeepsinghnitk@gmail.com>
2017-09-15 23:12:23 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,31 +1,4 @@
-# About Me
-Currently pursuing my Master's degree in Information Security from National Institute of Technology, Karnataka. Let me help you to Show some of my Work till date.Check Out My [C.V](https://github.com/pradeepsinghnitk/pradeepsinghnitk.github.io/blob/master/curriculum-vitae%20(2).pdf).
-<br>
-<br>
-<br>
-## Ongoing Project
-<br>
->Depth Analysis of Windows IOT and Exploitation Zigbee. Details will be updated as the project progresses.
-<br>
-<br>
-## Completed Projects
-<br>
-### Link fixer
+# Link fixer #
 
-> This Project aims to Protection of user privacy by preventing google from hijacking User  search result.[An extension](https://github.com/dblackmorris/fixer.git) for Chromium that prevents Google from hijacking your search results. [read more](http://ieeexplore.ieee.org/document/7764412/)
+An extension for Chromium that prevents Google from hijacking your search results.It is a client side privacy preserving protection.
 
-<br>
-<br>
-
-### Enhanced Sentiment Learning Using Twitter Hashtags and Smileys
-
-> A Data Analytical Project which aims to comparison between mobile carriers company and cyber vulnerability analysis. [read more](http://www.aclweb.org/anthology/C10-2028)
-
-<br>
-<br>
-
-<header><h4><p align="right">Contact</p> </h4></header> 
-
-<p align="right"><a href="https://www.linkedin.com/in/pradeep-singh-1a0335119/">Linkedin</a></p> 
- <p align="right"><a href="https://www.facebook.com/pradeep.royal32">Facebook</a></p>
- <p align="right"><a href="https://github.com/pradeepsinghnitk">Github</a></p>
---
+++ content.js
@@ -0,0 +1,70 @@
+
+var actualCode = '(' + function() {
+    'use strict';
+
+    var found = null;
+    var restore = function() {
+        try {
+            if (found && ('getAttribute' in found.element) && found.element.getAttribute('href') !== found.href) {
+                found.element.href = found.href;
+            }
+        } catch(e) {
+            // ignored
+        }
+    };
+
+    // Fix the links by adding a pair of mousedown handlers
+    window.addEventListener('mousedown', function(e) {
+        // Find the <a> tag
+        var curr = null;
+        for (curr = e.target;
+             curr && curr.localName && curr.localName.toLowerCase() !== 'a';
+             curr = curr.parentNode) {
+        }
+
+        // Verify it is in the search container, break out if the encrypted.google iframe if necessary
+        var checked = false;
+        if (curr) {
+            var par = curr.parentNode;
+            if (window.frameElement) {
+                par = window.frameElement.parentNode;
+            }
+
+            for (; par && par.parentNode; par = par.parentNode) {
+                if (('getAttribute' in par) && par.getAttribute('id') === 'search') {
+                    checked = true;
+                    break;
+                }
+            }
+        }
+
+        if (!checked) {
+            curr = null;
+        }
+
+        // Didn't find it
+        if (!curr) {
+            found = null;
+            return;
+        }
+
+        // Save the link location
+        found = {
+            'href': curr.href,
+            'element': curr
+        };
+
+        // Restore even if event was cancelled
+        setTimeout(restore, 0);
+    }, true);
+
+    window.addEventListener('mousedown', restore, false);
+
+} + ')();';
+
+// Inject code into the page to access the underlying JavaScript objects
+// https://stackoverflow.com/questions/9515704/building-a-chrome-extension-inject-code-in-a-page-using-a-content-script
+var script = document.createElement('script');
+script.textContent = actualCode;
+(document.head || document.documentElement).appendChild(script);
+script.parentNode.removeChild(script);
